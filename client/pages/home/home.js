239681ee6395c108376ea33a7c9269104ed2ab99
Blank <huyanpinh_113@163.com>
2018-06-08 23:25:39 +0800

    first commit

---
+++ client/pages/home/home.js
@@ -0,0 +1,135 @@
+// pages/home/home.js
+const qcloud = require('../../vendor/wafer2-client-sdk/index')
+const config = require('../../config.js')
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    productList: [], // 商品列表
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log('进入 onLoad 状态，准备抓取商品信息')
+    this.getProductList()
+    console.log(this.data)
+  },
+
+  getProductList(){
+    wx.showLoading({
+      title: '商品数据加载中...',
+    })
+    qcloud.request({
+      url: config.service.productList,
+      success: result => {
+        wx.hideLoading()
+        if (!result.data.code){
+          this.setData({
+            productList: result.data.data
+          })
+        }else {
+          wx.showToast({
+            icon: 'none',
+            title: '商品数据加载错误',
+          })
+        }
+      },
+      fail: result => {
+        wx.hideLoading()
+        wx.showToast({
+          icon: 'none',
+          title: '商品数据加载错误',
+        })
+      }
+    })
+  },
+
+  addToTrolley(event){
+    let productId = event.currentTarget.dataset.id
+
+    if (productId){
+      qcloud.request({
+        url: config.service.addTrolley,
+        login: true,
+        method: 'PUT',
+        data: {
+          id: productId
+        },
+        success: result => {
+          let data = result.data
+
+          if(!data.code) {
+            wx.showToast({
+              title: '已添加到购物车',
+            })
+          } else {
+            wx.showToast({
+              icon: 'none',
+              title: '添加到购物车失败',
+            })
+          }
+        },
+        fail: () => {
+          wx.showToast({
+            icon: 'none',
+            title: '添加到购物车失败',
+          })
+        }
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
